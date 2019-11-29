1613cd9f0eb20866ec51d97a60be90c3d311b2c3
wpj <2997261975@qq.com>
2019-11-29 08:37:09 +0800

    完成 我的 页面

--- app/src/main/java/com/example/coolvideo/ui/me/MeFragment.kt
+++ app/src/main/java/com/example/coolvideo/ui/me/MeFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.coolvideo.R
+import com.qmuiteam.qmui.widget.grouplist.QMUICommonListItemView
+import com.qmuiteam.qmui.widget.grouplist.QMUIGroupListView
 
 class MeFragment : Fragment() {
 
@@ -22,10 +24,32 @@ class MeFragment : Fragment() {
         meViewModel =
             ViewModelProviders.of(this).get(MeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_me, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        meViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
+        initGroupList(root)
         return root
+    }
+
+    private fun initGroupList(root: View) {
+        var meList: QMUIGroupListView= root.findViewById(R.id.meList)
+        val history=meList.createItemView(
+            ContextCompat.getDrawable(context!!, R.mipmap.tubiao),
+            "观看记录",
+            null,
+            QMUICommonListItemView.HORIZONTAL,
+            QMUICommonListItemView.ACCESSORY_TYPE_CHEVRON)
+        val collection=meList.createItemView(
+            ContextCompat.getDrawable(context!!, R.mipmap.tubiao),
+            "我的收藏",
+            null,
+            QMUICommonListItemView.HORIZONTAL,
+            QMUICommonListItemView.ACCESSORY_TYPE_CHEVRON)
+        val onClickListener: View.OnClickListener=View.OnClickListener { v->
+            Toast.makeText(activity,"click",Toast.LENGTH_SHORT).show()
+        }
+
+        QMUIGroupListView.newSection(context)
+            .addItemView(history,onClickListener)
+            .addItemView(collection,onClickListener)
+            .addTo(meList)
     }
 }
