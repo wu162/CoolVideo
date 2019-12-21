dccd273550f63913a76a930d7acf7dbc50681cc4
wpj <2997261975@qq.com>
2019-12-21 16:52:57 +0800

    完成选择头像，开始添加上传到服务器

--- app/src/main/java/com/example/coolvideo/ui/EditInfo/EditInfoActivity.kt
+++ app/src/main/java/com/example/coolvideo/ui/EditInfo/EditInfoActivity.kt
@@ -1,40 +1,81 @@
 package com.example.coolvideo.ui.EditInfo
 
+import android.Manifest
+import android.app.Activity
 import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
 import com.example.coolvideo.R
+import com.example.coolvideo.databinding.ActivityEditinfoBinding
 import com.qmuiteam.qmui.widget.QMUIRadiusImageView
 import com.qmuiteam.qmui.widget.dialog.QMUIDialog
 import com.qmuiteam.qmui.widget.grouplist.QMUICommonListItemView
 import com.qmuiteam.qmui.widget.grouplist.QMUIGroupListView
-import kotlinx.android.synthetic.main.activity_editinfo.*
+import com.tbruyelle.rxpermissions2.RxPermissions
+import com.zhihu.matisse.Matisse
+import com.zhihu.matisse.MimeType
+import com.zhihu.matisse.engine.impl.GlideEngine
+import java.io.File
 
 class EditInfoActivity : AppCompatActivity() {
     private lateinit var editInfoViewModel: EditInfoViewModel
-    val mCurrentDialogStyle=com.qmuiteam.qmui.R.style.QMUI_Dialog
+    private lateinit var binding : ActivityEditinfoBinding
+    private lateinit var avatarPath:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_editinfo)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_editinfo)
 
         editInfoViewModel =
             ViewModelProviders.of(this).get(EditInfoViewModel::class.java)
 
+        avatarPath=""
         initChangeUserImg()
         initInfoList()
+        initSubmitListener()
+    }
+
+    private fun initSubmitListener() {
+        binding.infoSubmit.setOnClickListener {
+            var file=File(avatarPath)
+
+        }
     }
 
     private fun initChangeUserImg() {
         val userImg=findViewById<QMUIRadiusImageView>(R.id.meedit_user_img)
         userImg.setOnClickListener {
             Log.i("img","click")
+            var rxPermissions = RxPermissions(this)
+            rxPermissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .subscribe(
+                    { aBoolean: Boolean ->
+                        if (aBoolean) {
+                            ChooseAvatar()
+                        } else {
+                            Toast.makeText(
+                                this,
+                                R.string.permission_request_denied,
+                                Toast.LENGTH_LONG
+                            )
+                                .show()
+                        }
+                    },
+                    { obj: Throwable -> obj.printStackTrace() }
+                )
+
 //            val items=arrayOf("更改头像")
 //            QMUIDialog.MenuDialogBuilder(this)
 //                .addItems(items) { dialog: DialogInterface, which: Int ->
@@ -45,6 +86,49 @@ class EditInfoActivity : AppCompatActivity() {
 
         }
     }
+
+    private fun ChooseAvatar() {
+        Matisse.from(this)
+            .choose(MimeType.ofAll())
+            .countable(true)
+            .maxSelectable(1)
+            .gridExpectedSize(
+                getResources().getDimensionPixelSize(R.dimen.grid_expected_size))
+            .restrictOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+            .thumbnailScale(0.85f)
+            .imageEngine(GlideEngine())
+            .forResult(REQUEST_CODE_CHOOSE)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) { // 操作成功了
+            when (requestCode) {
+                REQUEST_CODE_CHOOSE -> {
+                    val imglisturi = Matisse.obtainResult(data)
+                    Log.i("onActivityResult",imglisturi[0].toString())
+                    var filepath=getRealPath(imglisturi[0])
+                    Log.i("onActivityResult",filepath)
+                    Glide.with(this).load(imglisturi[0]).into(binding.meeditUserImg)
+                }
+            }
+        }
+    }
+
+    private fun getRealPath(uri: Uri): String {
+        var path = uri.path
+        var split = path!!.split("/")
+
+        var filePath = getMatissePhotoPath() + File.separatorChar + split[split.size - 1]
+        return filePath
+    }
+
+    private fun getMatissePhotoPath(): String {
+        var file = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        return file.absolutePath
+    }
+
 
     private fun initInfoList() {
         var meList = findViewById<QMUIGroupListView>(R.id.meedit_list)
@@ -63,8 +147,14 @@ class EditInfoActivity : AppCompatActivity() {
             .addTo(meList)
     }
 
+
+
     override fun finish() {
         super.finish()
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+
+    companion object{
+        private const val REQUEST_CODE_CHOOSE = 23
     }
 }
